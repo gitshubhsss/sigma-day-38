3184b4cc2caaa3bc48dc9ffba93c5dd1f9d52303
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-22 17:03:38 +0530

    forEach funtion in js

---
+++ ArrayMethods/forEach.js
@@ -0,0 +1,17 @@
+let numbers=[1,5,8,7,6];
+numbers.forEach(printNum=(num)=>{
+    console.log(num*2);
+})
+let arr=[{
+    name:"shubham",
+    marks:84
+},{
+    name:"avantika",
+    marks:84
+},{
+    name:"shraddha",
+    marks:84
+}]
+arr.forEach(printName=(names)=>{
+    console.log(names.name);
+})
---
+++ ArrayMethods/forEach2.js
@@ -0,0 +1,13 @@
+let loveArr=[{
+    girlFriend:"avantika",
+    boyFriend:"sujal"
+},{
+    girlFriend:"bhumika",
+    boyFriend:"shubham"
+},{
+    girlFriend:"monali",
+    boyFriend:"omkar"
+}];
+loveArr.forEach((boyFriend)=>{
+   console.log(boyFriend.boyFriend);
+})
